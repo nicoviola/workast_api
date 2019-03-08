fad298c4f68bbe2afc21c125fc4a0ac0f113ff13
nicoviola <47794806+nicoviola@users.noreply.github.com>
2019-03-08 19:06:18 -0300

    Revert "Wa 2 add models"

--- database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://mongo:27017/workast',() => {
-        useNewUrlParser: true
-    }).then(db => console.log('db connected'))
-      .catch(err => console.log(err))
