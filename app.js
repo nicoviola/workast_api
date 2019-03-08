4aa29e5962e5d467e7e0a66a861f21858b76a7c3
Nicolas <nicolas.viola@ubykuo.com>
2019-03-08 17:49:51 -0300

    WA-1 added Router and morgan dev

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const indexRouter = require('./routes/index')
 
 const app = express()
-
-
 app.set('port', 3000)
 
-app.use(morgan())
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use('/', indexRouter)
+
 
 app.listen(app.get('port'), ()=>{
     console.log('listening on port ',app.get('port') )
