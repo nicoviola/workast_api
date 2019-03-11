455d5767817db169fa376bc1689cbfa51447f655
Nicolas <nicolas.viola@ubykuo.com>
2019-03-11 20:39:50 -0300

    WA-8 added article search endpoint, added article search controller behaviour

--- routes/articles.js
+++ routes/articles.js
@@ -9,19 +9,25 @@ let validate = require('../middlewares/validate')
  */
 router.get('/', articleController.list)
 
-/*** POST article
+/*** POST articles
  * Create an article
  */
 router.post('/', validate(articleSchemes.createArticle), articleController.createArticle)
 
-/*** POST article/:id
+/*** POST articles/:id
  * Updates an already existing article
  */
 router.post('/:id([0-9a-fA-F]{24})', validate(articleSchemes.updateArticle), articleController.updateArticle)
 
-/*** DELETE article/:id
+/*** DELETE articles/:id
  * Deletes an already existing article
  */
 router.delete('/:id([0-9a-fA-F]{24})',  articleController.deleteArticle)
+
+
+/*** GET articles/search
+ * Search for tags
+ */
+router.get('/search',  articleController.search)
 
 module.exports = router
